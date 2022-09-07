dcd3e7904b2c93b891ef4526e4b330a2d0934f18
BillyMiles <williammiles201@gmail.com>
2022-09-07 11:30:57 -0400

    menu async resolved

--- src/Menu.js
+++ src/Menu.js
@@ -4,17 +4,16 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 
-const Menu = () => {
+function Menu() {
     const navigate = useNavigate()
-    const [ categories, setCategories ] = useState([])
-    const [ selection, setSelection ] = useState([])
+    let [ categories, setCategories ] = useState([])
+    let [ selection, setSelection ] = useState([])
 
     useEffect(() => { 
         fetch('https://opentdb.com/api_category.php')
         .then((res) => res.json())
-        .then(res => console.log("RES", res))
         .then(res => setCategories(res.trivia_categories))
-        })
+    })
 
         const handleSubmit = (e) => {
             e.preventDefault()
@@ -25,32 +24,39 @@ const Menu = () => {
             e.preventDefault()
             navigate("/question")
         }
+        
+        if (categories.length > 0)
+            
+            return (
+                <div>
+                <h2>Menu</h2> 
+                    <p>Category Selections:</p>
 
-        return (
-            <div>
-            <h2>Menu</h2> 
-                <p>Category Selections:</p>
-                <Dropdown>
+                    <Dropdown>
 
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Dropdown Button
-                    </Dropdown.Toggle>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Select Category
+                        </Dropdown.Toggle>
 
-                    <Dropdown.Menu >
+                        <Dropdown.Menu >
 
-                        {categories.forEach((cat)=> (
-                        <Dropdown.Item name={cat.name} onClick={handleSubmit}>{cat.name}</Dropdown.Item>
-                        ))}
-                        
-                    </Dropdown.Menu>
+                            {categories.map((cat)=> (
+                            <Dropdown.Item name={cat.name} onClick={handleSubmit}>{cat.name}</Dropdown.Item>
+                            ))}
+                            
+                        </Dropdown.Menu>
 
-                </Dropdown>
-
-                <br/>
-                
-                {selection.length > 0 ? <Button onClick={handlePlay} category={selection}>Play: {selection}</Button> : null}
-            </div>
-        );
+                    </Dropdown>
+    
+                    <br/>
+                    
+                    {selection.length > 0 ? <Button onClick={handlePlay} category={selection}>Play: {selection}</Button> : null}
+                </div>
+            );
+        else 
+            return (
+                <div> LOADING </div>
+            )
 }
 
 export default Menu;
